048b08c261a5537ab0378626ae8e849f18613505
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-09-28 17:41:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :students 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student' 
 end
+
+
+
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   resources :posts, only: [:index, :show]
+# end
+
